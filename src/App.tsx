964eb4d7eedd530e7c2126091df23fa5a3e67364
lucabedford <lucacybedford@gmail.com>
2025-02-18 18:42:31 +0000

    made scrollable div

--- src/App.tsx
+++ src/App.tsx
@@ -991,19 +991,38 @@ function App(): ReactElement {
                 <div id={"module-selector-container"}>
                     <div id={"module-selector"}>
                         <div id={"scrollable-module-container"}>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
-                            <div className={"mock-module"}></div>
+                            <div className={"horizontal-module-container"}>
+                                <div className={"mock-module"}></div>
+                                <div className={"mock-module"}></div>
+                            </div>
+                            <div className={"horizontal-module-container"}>
+                                <div className={"mock-module"}></div>
+                                <div className={"mock-module"}></div>
+                            </div>
+                            <div className={"horizontal-module-container"}>
+                                <div className={"mock-module"}></div>
+                                <div className={"mock-module"}></div>
+                            </div>
+                            <div className={"horizontal-module-container"}>
+                                <div className={"mock-module"}></div>
+                                <div className={"mock-module"}></div>
+                            </div>
+                            <div className={"horizontal-module-container"}>
+                                <div className={"mock-module"}></div>
+                                <div className={"mock-module"}></div>
+                            </div>
+                            <div className={"horizontal-module-container"}>
+                                <div className={"mock-module"}></div>
+                                <div className={"mock-module"}></div>
+                            </div>
+                            <div className={"horizontal-module-container"}>
+                                <div className={"mock-module"}></div>
+                                <div className={"mock-module"}></div>
+                            </div>
+                            <div className={"horizontal-module-container"}>
+                                <div className={"mock-module"}></div>
+                                <div className={"mock-module"}></div>
+                            </div>
                         </div>
                     </div>
                 </div>
